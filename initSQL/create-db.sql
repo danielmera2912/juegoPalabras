22b75da38efcb32086c554bee5f0c10ed6578bcc
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-03-05 22:05:19 +0100

    correccion script

--- initSQL/create-db.sql
+++ initSQL/create-db.sql
@@ -18,82 +18,84 @@ USE `juegoPalabra` ;
 -- Table `juegoPalabra`.`equipo`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juegoPalabra`.`equipo` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `logo` VARCHAR(100) NOT NULL,
-  `fecha_creacion` DATE NULL,
-  `fecha_modificacion` DATE NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+                                                       `id` INT NOT NULL AUTO_INCREMENT,
+                                                       `nombre` VARCHAR(45) NOT NULL,
+    `logo` VARCHAR(100) NOT NULL,
+    `fecha_creacion` DATE NOT NULL,
+    `fecha_modificacion` DATE NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `juegoPalabra`.`jugador`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juegoPalabra`.`jugador` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `clave` VARCHAR(45) NOT NULL,
-  `avatar` VARCHAR(100) NOT NULL,
-  `id_equipo` INT NOT NULL,
-  `rol` VARCHAR(45) NULL,
-  `correo` VARCHAR(50) NULL,
-  `fecha_creacion` DATE NULL,
-  `fecha_modificacion` DATE NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Jugador_Equipo1_idx` (`id_equipo` ASC) VISIBLE,
-  CONSTRAINT `fk_Jugador_Equipo1`
+                                                        `id` INT NOT NULL AUTO_INCREMENT,
+                                                        `nombre` VARCHAR(45) NOT NULL,
+    `clave` VARCHAR(45) NOT NULL,
+    `avatar` VARCHAR(100) NOT NULL,
+    `id_equipo` INT NOT NULL,
+    `rol` VARCHAR(45) NOT NULL,
+    `correo` VARCHAR(50) NOT NULL,
+    `fecha_creacion` DATE NOT NULL,
+    `fecha_modificacion` DATE NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_Jugador_Equipo1_idx` (`id_equipo` ASC) VISIBLE,
+    UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE,
+    CONSTRAINT `fk_Jugador_Equipo1`
     FOREIGN KEY (`id_equipo`)
     REFERENCES `juegoPalabra`.`equipo` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `juegoPalabra`.`juego`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juegoPalabra`.`juego` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `intentos` TINYINT(2) NOT NULL,
-  `dificultad` ENUM("facil", "normal", "dificil") NOT NULL,
-  `instrucciones` VARCHAR(500) NULL,
-  `fecha_modificacion` DATE NULL,
-  `fecha_creacion` DATE NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+                                                      `id` INT NOT NULL AUTO_INCREMENT,
+                                                      `nombre` VARCHAR(45) NOT NULL,
+    `intentos` TINYINT(2) NOT NULL,
+    `dificultad` ENUM("facil", "normal", "dificil") NOT NULL,
+    `instrucciones` VARCHAR(500) NOT NULL,
+    `fecha_modificacion` DATE NOT NULL,
+    `fecha_creacion` DATE NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `juegoPalabra`.`partida`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juegoPalabra`.`partida` (
-  `id_jugador` INT NOT NULL,
-  `id_juego` INT NOT NULL,
-  `intentos` TINYINT(2) NOT NULL,
-  `palabra` VARCHAR(45) NOT NULL,
-  `puntos` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  PRIMARY KEY (`id_jugador`, `id_juego`),
-  INDEX `fk_Jugador_has_Juego_Juego1_idx` (`id_juego` ASC) VISIBLE,
-  INDEX `fk_Jugador_has_Juego_Jugador_idx` (`id_jugador` ASC) VISIBLE,
-  CONSTRAINT `fk_Jugador_has_Juego_Jugador`
+                                                        `id_jugador` INT NOT NULL,
+                                                        `id_juego` INT NOT NULL,
+                                                        `intentos` TINYINT(2) NOT NULL,
+    `palabra` VARCHAR(45) NOT NULL,
+    `puntos` INT NOT NULL,
+    `fecha` DATE NOT NULL,
+    PRIMARY KEY (`id_jugador`, `id_juego`),
+    INDEX `fk_Jugador_has_Juego_Juego1_idx` (`id_juego` ASC) VISIBLE,
+    INDEX `fk_Jugador_has_Juego_Jugador_idx` (`id_jugador` ASC) VISIBLE,
+    CONSTRAINT `fk_Jugador_has_Juego_Jugador`
     FOREIGN KEY (`id_jugador`)
     REFERENCES `juegoPalabra`.`jugador` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Jugador_has_Juego_Juego1`
+    CONSTRAINT `fk_Jugador_has_Juego_Juego1`
     FOREIGN KEY (`id_juego`)
     REFERENCES `juegoPalabra`.`juego` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 
 -- Datos para la tabla equipo
