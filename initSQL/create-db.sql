748979d528dff5fc26487e5beef88f7441cfaddc
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-03-07 17:14:20 +0100

    pequeño ajuste

--- initSQL/create-db.sql
+++ initSQL/create-db.sql
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `juegoPalabra`.`juego` (
                                                       `id` INT NOT NULL AUTO_INCREMENT,
                                                       `nombre` VARCHAR(45) NOT NULL,
     `intentos` TINYINT(2) NOT NULL,
-    `dificultad` ENUM("facil", "normal", "dificil") NOT NULL,
+    `dificultad` ENUM("FACIL", "NORMAL", "DIFICIL") NOT NULL,
     `instrucciones` VARCHAR(500) NOT NULL,
     `fecha_modificacion` DATE NOT NULL,
     `fecha_creacion` DATE NOT NULL,
@@ -73,22 +73,22 @@ CREATE TABLE IF NOT EXISTS `juegoPalabra`.`juego` (
 -- Table `juegoPalabra`.`partida`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juegoPalabra`.`partida` (
-                                                        `id_jugador` INT NOT NULL,
-                                                        `id_juego` INT NOT NULL,
+                                                        `jugador_id` INT NOT NULL,
+                                                        `juego_id` INT NOT NULL,
                                                         `intentos` TINYINT(2) NOT NULL,
     `palabra` VARCHAR(45) NOT NULL,
     `puntos` INT NOT NULL,
     `fecha` DATE NOT NULL,
-    PRIMARY KEY (`id_jugador`, `id_juego`),
-    INDEX `fk_Jugador_has_Juego_Juego1_idx` (`id_juego` ASC) VISIBLE,
-    INDEX `fk_Jugador_has_Juego_Jugador_idx` (`id_jugador` ASC) VISIBLE,
+    PRIMARY KEY (`jugador_id`, `juego_id`),
+    INDEX `fk_Jugador_has_Juego_Juego1_idx` (`juego_id` ASC) VISIBLE,
+    INDEX `fk_Jugador_has_Juego_Jugador_idx` (`jugador_id` ASC) VISIBLE,
     CONSTRAINT `fk_Jugador_has_Juego_Jugador`
-    FOREIGN KEY (`id_jugador`)
+    FOREIGN KEY (`jugador_id`)
     REFERENCES `juegoPalabra`.`jugador` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
     CONSTRAINT `fk_Jugador_has_Juego_Juego1`
-    FOREIGN KEY (`id_juego`)
+    FOREIGN KEY (`juego_id`)
     REFERENCES `juegoPalabra`.`juego` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION
@@ -104,26 +104,34 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- Datos para la tabla equipo
 INSERT INTO juegoPalabra.equipo (nombre, logo, fecha_creacion, fecha_modificacion) VALUES
-('Rojo', 'https://example.com/rojo_logo.png', '2022-01-01', '2022-01-02'),
-('Azul', 'https://example.com/azul_logo.png', '2022-01-01', '2022-01-02'),
-('Verde', 'https://example.com/verde_logo.png', '2022-01-01', '2022-01-02');
+                                                                                       ('Rojo', 'https://example.com/rojo_logo.png', '2022-01-01', '2022-01-02'),
+                                                                                       ('Azul', 'https://example.com/azul_logo.png', '2022-01-01', '2022-01-02'),
+                                                                                       ('Verde', 'https://example.com/verde_logo.png', '2022-01-01', '2022-01-02');
 
 -- Datos para la tabla jugador
 INSERT INTO juegoPalabra.jugador (nombre, clave, avatar, id_equipo, rol, correo, fecha_creacion, fecha_modificacion) VALUES
-('Jugador1', 'password1', 'https://example.com/avatar1.png', 1, 'user', 'jugador1@example.com', '2022-01-01', '2022-01-02'),
-('Jugador2', 'password2', 'https://example.com/avatar2.png', 1, 'user', 'jugador2@example.com', '2022-01-01', '2022-01-02'),
-('Jugador3', 'password3', 'https://example.com/avatar3.png', 2, 'user', 'jugador3@example.com', '2022-01-01', '2022-01-02'),
-('Jugador4', 'password4', 'https://example.com/avatar4.png', 2, 'admin', 'jugador4@example.com', '2022-01-01', '2022-01-02');
+                                                                                                                         ('Jugador1', 'password1', 'https://example.com/avatar1.png', 1, 'user', 'jugador1@example.com', '2022-01-01', '2022-01-02'),
+                                                                                                                         ('Jugador2', 'password2', 'https://example.com/avatar2.png', 1, 'user', 'jugador2@example.com', '2022-01-01', '2022-01-02'),
+                                                                                                                         ('Jugador3', 'password3', 'https://example.com/avatar3.png', 2, 'user', 'jugador3@example.com', '2022-01-01', '2022-01-02'),
+                                                                                                                         ('Jugador4', 'password4', 'https://example.com/avatar4.png', 2, 'admin', 'jugador4@example.com', '2022-01-01', '2022-01-02');
 
 -- Datos para la tabla juego
 INSERT INTO juegoPalabra.juego (nombre, intentos, dificultad, instrucciones, fecha_modificacion, fecha_creacion) VALUES
-('Juego1', 5, 'facil', 'Adivina la palabra con 5 intentos', '2022-01-01', '2022-01-02'),
-('Juego2', 3, 'normal', 'Adivina la palabra con 3 intentos', '2022-01-01', '2022-01-02'),
-('Juego3', 1, 'dificil', 'Adivina la palabra con 1 intento', '2022-01-01', '2022-01-02');
+                                                                                                                     ('Juego1', 5, 'FACIL', 'Adivina la palabra con 5 intentos', '2022-01-01', '2022-01-02'),
+                                                                                                                     ('Juego2', 3, 'NORMAL', 'Adivina la palabra con 3 intentos', '2022-01-01', '2022-01-02'),
+                                                                                                                     ('Juego3', 1, 'DIFICIL', 'Adivina la palabra con 1 intento', '2022-01-01', '2022-01-02');
 
 -- Datos para la tabla partida
-INSERT INTO juegoPalabra.partida (id_jugador, id_juego, intentos, palabra, puntos, fecha) VALUES
-(1, 1, 5, 'perro', 50, '2022-01-01'),
-(1, 2, 3, 'gato', 70, '2022-01-01'),
-(2, 1, 5, 'casa', 30, '2022-01-01'),
-(3, 3, 1, 'helicóptero', 100, '2022-01-01');
+INSERT INTO juegoPalabra.partida (jugador_id, juego_id, intentos, palabra, puntos, fecha) VALUES
+                                                                                              (1, 1, 5, 'perro', 50, '2022-01-01'),
+                                                                                              (1, 2, 3, 'gato', 70, '2022-01-01'),
+                                                                                              (2, 1, 5, 'casa', 30, '2022-01-01'),
+                                                                                              (3, 3, 1, 'helicóptero', 100, '2022-01-01');
+
+-- Datos para la tabla juego
+
+INSERT INTO juegoPalabra.juego (nombre, intentos, dificultad, instrucciones, fecha_modificacion, fecha_creacion)
+VALUES
+    ('Palabras Fáciles', 3, 'FACIL', 'Adivina las palabras fáciles', '2022-03-01', '2022-02-28'),
+    ('Palabras Normales', 5, 'NORMAL', 'Adivina las palabras de dificultad normal', '2022-03-02', '2022-02-28'),
+    ('Palabras Difíciles', 7, 'DIFICIL', 'Adivina las palabras difíciles', '2022-03-03', '2022-02-28');
